9aee1ced6ab84c5052e01c2800949b7757e4e2cd
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-16 21:55:30 +0600

    login form creating

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 const Login = () => {
     return (
-        <div>
-            <h3>This is login</h3>
-        </div>
+      <div className="form-container">
+        <h3 className="form-title">Login</h3>
+        <form>
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id=""
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id=""
+              placeholder="Enter Your password"
+              required
+            />
+          </div>
+          <input type="submit" value="Login" className="btn-login" />
+        </form>
+        <p>
+          New to Ema-Jhon?{" "}
+          <Link to="/signup" style={{ color: "rgba(255, 153, 0, 1)" }}>
+            Create new Account
+          </Link>
+        </p>
+      </div>
     );
 };
 
